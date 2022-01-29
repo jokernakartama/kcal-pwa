67801a8f49bcb783d3cf452f70a6be074a6c2289
zzap <s.kobzev@yahoo.com>
2022-01-29 21:35:53 +0300

    Add a checkbox and a radio button

---
+++ src/components/ui/CheckboxInput/CheckboxInput.tsx
@@ -0,0 +1,22 @@
+import classNames from 'classnames'
+import { Component, JSX, splitProps } from 'solid-js'
+import styles from './styles.sass'
+
+type CheckboxInputComponent = Component<
+  Omit<JSX.IntrinsicElements['input'], 'type'>
+>
+
+/**
+ * Renders a checkbox field
+ */
+export const CheckboxInput: CheckboxInputComponent = props => {
+  const [local, rest] = splitProps(props, ['className', 'children'])
+
+  return (
+    <label className={classNames(styles.wrapper, local.className)}>
+      <input type="checkbox" {...rest} />
+      <div className={styles.switch}></div>
+      <span className={styles.label}>{local.children}</span>
+    </label>
+  )
+}
--- src/components/ui/RadioInput/RadioInput.tsx
+++ src/components/ui/RadioInput/RadioInput.tsx
@@ -1,20 +1,52 @@
 import classNames from 'classnames'
-import { Component, JSX } from 'solid-js'
+import {
+  Component,
+  createMemo,
+  createSelector,
+  For,
+  JSX,
+  splitProps
+} from 'solid-js'
 import styles from './styles.sass'
 
 type RadioInputComponent = Component<
-  Omit<JSX.IntrinsicElements['input'], 'type'>
+  Omit<JSX.IntrinsicElements['input'], 'type'> & {
+    options: Array<{
+      value?: string | number
+      label: JSX.Element
+      disabled?: boolean
+    }>
+  }
 >
 
 /**
- * Renders a radio button
+ * Renders a set of radio buttons
  */
-export const RadioInput: RadioInputComponent = ({ className, ...rest }) => {
+export const RadioInput: RadioInputComponent = props => {
+  const [local, rest] = splitProps(props, [
+    'className',
+    'children',
+    'options',
+    'value'
+  ])
+  const selected = createMemo(() => local.value)
+  const isSelected = createSelector(selected)
+
   return (
-    <input
-      type="radio"
-      className={classNames(styles.wrapper, className)}
-      {...rest}
-    />
+    <For each={local.options}>
+      {o => (
+        <label className={classNames(styles.wrapper, local.className)}>
+          <input
+            type="radio"
+            checked={isSelected(o.value)}
+            value={o.value}
+            disabled={o.disabled}
+            {...rest}
+          />
+          <div className={styles.switch}></div>
+          <span className={styles.label}>{o.label}</span>
+        </label>
+      )}
+    </For>
   )
 }
