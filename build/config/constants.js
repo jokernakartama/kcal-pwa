8d7774cfcbedb1b63ed2e539e7a92c5564cb29d4
zzap <s.kobzev@yahoo.com>
2022-02-03 21:05:54 +0300

    Add an action to deploy the app

--- build/config/constants.js
+++ build/config/constants.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const rootDir = path.join(__dirname, '../../')
-const buildPath = path.join(rootDir, 'dist')
+const buildPath = path.join(rootDir, 'docs')
 const staticDir = path.join(rootDir, 'static')
 
 module.exports = {
