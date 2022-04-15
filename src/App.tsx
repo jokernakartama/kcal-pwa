0f2a53e08240f82dd891b9d34407652fbfb90651
zzap <s.kobzev@yahoo.com>
2022-04-15 20:41:59 +0300

    Disable DBWorkerProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Component, createSignal, onMount, Show } from 'solid-js'
 import { Layout } from './components/layout/Layout'
-import { DBWorkerProvider } from './db'
 import { I18nProvider } from './i18n'
 import { i18n } from './i18n/config'
 import { StoreProvider } from './store'
@@ -21,13 +20,11 @@ export const App: Component = () => {
 
   return (
     <Show when={isReady()}>
-      <DBWorkerProvider>
-        <I18nProvider>
-          <StoreProvider>
-            <Layout />
-          </StoreProvider>
-        </I18nProvider>
-      </DBWorkerProvider>
+      <I18nProvider>
+        <StoreProvider>
+          <Layout />
+        </StoreProvider>
+      </I18nProvider>
     </Show>
   )
 }
--- src/components/views/GettingStarted/GettingStarted.tsx
+++ src/components/views/GettingStarted/GettingStarted.tsx
@@ -1,28 +1,15 @@
 import { Component } from 'solid-js'
-import { useDB } from '../../../db'
+// import { useStore } from '../../../store'
 import { UserInfoForm } from '../../forms/UserInfoForm'
 import { Container } from '../../layout/Grid'
-import { Button } from '../../ui/Button'
 
 export const GettingStarted: Component = () => {
-  const { dispatch } = useDB()
   function handleSubmit(values: UserModel.Info) {
     console.info(values)
   }
 
-  function addBook() {
-    dispatch('ping')
-      .then(data => {
-        console.info(data)
-      })
-      .catch((err) => {
-        console.warn(err)
-      })
-  }
-
   return (
     <Container>
-      <Button color="accent" onClick={addBook}>Ping</Button>
       <UserInfoForm onSubmit={handleSubmit} />
     </Container>
   )
