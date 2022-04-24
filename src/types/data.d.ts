fb351e8a69cfc96fdee1421d0d18f48fedd402c0
zzap <s.kobzev@yahoo.com>
2022-04-24 14:58:34 +0300

    Change data types

--- src/types/data.d.ts
+++ src/types/data.d.ts
@@ -1,40 +1,83 @@
 declare namespace DataModel {
   export type Mass = number
 
+  /**
+   * Base type for nutrition
+   */
   export interface Nutrition {
+    /** Amount of proteins in grams */
     proteins: number
+    /**  Amount of fats in grams */
     fats: number
+    /**  Amount of carbs in grams  */
     carbohydrates: number
+    /**  Energy in kcal */
     kcalories: number
-    // micro elements
   }
 
+  /**
+   * Nutrition of 100 grams of the product
+   */
   export interface Product extends Nutrition {
-    userId: UserModel.Info['id']
     id: number
+    /**  Bound user's id */
+    userId: UserModel.Info['id']
+    /**  The name of the product */
     name: string
   }
 
+  /**
+   * A set of products with defined mass
+   */
   export interface Recipe {
-    userId: UserModel.Info['id']
     id: number
+    /**  Bound user's id */
+    userId: UserModel.Info['id']
+    /**  The name of the recipe */
     name: string
-    products: Array<{ id: Product['id']; mass: Mass }>
+    /**  Bound products */
+    products: Array<
+      Product &
+      {
+        /** The mass of the product */
+        mass: Mass
+        /** Whether the product has been removed */
+        isArchived?: boolean
+      }
+    >
+    /** A short description of the recipe */
     description?: string
   }
 
+  /**
+   * One time meal record. It can contains either product or recipe.
+   */
   export interface Meal {
-    dayId: DataModel.JournalRecord['id']
-    date: string
-    recipeId?: Product['id']
-    productId?: Recipe['id']
+    id: number
+    /** Bound user's id */
+    userId: UserModel.Info['id']
+    /** Bound journal record */
+    recordId: DataModel.JournalRecord['id']
+    /** Datetime stamp */
+    time: Date
+    /** Bound recipe object */
+    recipe?: Omit<Recipe, 'userId'>
+    /** Bound product object */
+    product?: Product
+    /** Whether the bound entity has been removed */
+    isAchived?: boolean
+    /**
+     * Resulting mass of the product or meal by recipe. If it's a recipe,
+     * calculated mass of the meal should be used by default.
+     */
     mass: Mass
   }
 
   export interface JournalRecord {
-    userId: UserModel.Info['id']
     id: number
-    date: Date
-    meal: Meal[]
+    /** Bound user's id */
+    userId: UserModel.Info['id']
+    /** A date in YYYY-MM-DD format */
+    date: string
   }
 }
