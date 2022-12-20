666a5b17bb4eb104089c2472e0dfdb418581ac80
zzap <s.kobzev@yahoo.com>
2022-12-20 22:48:22 +0300

    Change user data model

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -22,7 +22,7 @@ declare namespace UserModel {
     carbohydrates: number
   }
 
-  export interface Personal {
+  export interface User {
     id: number
     /**
      * The user's name
@@ -40,10 +40,6 @@ declare namespace UserModel {
      * The user's height in centimeters
      */
     height: number
-  }
-
-  export interface Status {
-    userId: Info['id']
     /**
      * Value of the user's weight in kilograms
      */
@@ -65,14 +61,12 @@ declare namespace UserModel {
      * - 0 - to keep weight
      * - 0.15 - to gain weight
      */
-    goal: WeightGoal
+    massGainDir: -1 | 0 | 1
     /**
      * - Normal 0.3 0.3 0.4
      * - To cut 0.4 0.25 0.35 (0.4 0.2-0.25 0.35-0.4)
      * - To bulk 0.3 0.2 0.5 (0.3-0.4 0.2-0.25 0.4-0.5)
      */
-    nutritionType: NutritionType
-
-    customNutrition?: [number, number, number]
+    nutrientsRatio: [number, number, number]
   }
 }
