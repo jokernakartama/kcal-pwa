5d6681c9d3c361d32a7ce94b7c3659fa3b22e6b6
zzap <s.kobzev@yahoo.com>
2023-01-13 20:10:39 +0300

    Add form type

---
+++ src/components/views/GettingStarted/types.ts
@@ -0,0 +1,10 @@
+export interface GettingStartedFormFields {
+  name: string
+  birthDate: string
+  sex: UserModel.User['sex']
+  height: UserModel.User['height']
+  weight: UserModel.User['weight']
+  activity: UserModel.User['activity']
+  massGainDir: UserModel.User['massGainDir']
+  nutrientsRatio: string
+}
