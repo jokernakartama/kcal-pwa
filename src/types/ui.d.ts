83d25950852fcd1f4e4077adefe5c5d11b1f3c3d
zzap <s.kobzev@yahoo.com>
2023-01-15 11:17:29 +0300

    Add color name type

--- src/types/ui.d.ts
+++ src/types/ui.d.ts
@@ -1,3 +1,15 @@
 declare namespace UI {
   export type Color = 'primary' | 'secondary' | 'accent' | 'warning' | 'danger'
+
+  export type OriginalColorName =
+  | 'black'
+  | 'metal'
+  | 'gray'
+  | 'silver'
+  | 'white'
+  | 'blue'
+  | 'green'
+  | 'red'
+  | 'orange'
+  | 'yellow'
 }
