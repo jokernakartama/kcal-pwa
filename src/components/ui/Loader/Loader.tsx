858c170dc5b950c51d40d7333c0be98fa6f02395
zzap <s.kobzev@yahoo.com>
2023-01-21 18:31:58 +0300

    Add Loader component

---
+++ src/components/ui/Loader/Loader.tsx
@@ -0,0 +1,23 @@
+import classNames from 'classnames'
+import { Component, JSX, splitProps } from 'solid-js'
+import styles from './styles.sass'
+
+type LoaderComponent = Component<
+  JSX.IntrinsicElements['span'] & {
+    block?: boolean
+  }
+>
+
+/**
+ * Renders animated content replacement
+ */
+export const Loader: LoaderComponent = props => {
+  const [local, rest] = splitProps(props, ['block', 'class'])
+
+  return (
+    <span
+      class={classNames(styles.wrapper, local.class, { [styles.block]: local.block })}
+      {...rest}
+    />
+  )
+}
