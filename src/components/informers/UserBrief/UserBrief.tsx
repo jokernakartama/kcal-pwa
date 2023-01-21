7d630364bc3c4e5d97556e92616c02f7d8628e9e
zzap <s.kobzev@yahoo.com>
2023-01-21 21:43:08 +0300

    Add UserBrief informer

---
+++ src/components/informers/UserBrief/UserBrief.tsx
@@ -0,0 +1,50 @@
+import classNames from 'classnames'
+import { Component, createMemo, JSX, Match, Show, splitProps, Switch } from 'solid-js'
+import { emoji } from '../../../constants/emoji'
+import { useT } from '../../../i18n'
+import { useStore } from '../../../store'
+import { getDiffInYears } from '../../../utils/date'
+import { Loader } from '../../ui/Loader'
+import styles from './styles.sass'
+
+type UserBriefComponent = Component<
+  JSX.IntrinsicElements['div']
+>
+
+export const UserBrief: UserBriefComponent = props => {
+  const [local, rest] = splitProps(props, ['class'])
+  const t = useT()
+  const [store] = useStore()
+  const age = createMemo(() => {
+    if (!store.user?.birthDate) return 0
+
+    const birthdate = new Date(store.user.birthDate)
+
+    return Math.abs(Math.floor(getDiffInYears(birthdate, new Date())))
+  })
+
+  return (
+    <div class={classNames(styles.wrapper, local.class)} {...rest}>
+      <span class={styles.sex}>
+        <Switch fallback={<Loader style={{ width: '1.5em' }} />}>
+          <Match when={store.user?.sex === 'male'} >
+            {emoji.man.html}
+          </Match>
+          <Match when={store.user?.sex === 'female'} >
+            {emoji.woman.html}
+          </Match>
+        </Switch>
+      </span>
+      <span class={styles.name}>
+        <Show when={store.user?.name} fallback={<Loader style={{ width: '3em' }} />}>
+          {store.user!.name}
+        </Show>
+      </span>
+      <span class={styles.age}>
+        <Show when={age() > 0} fallback={<Loader style={{ width: '2em' }} />}>
+          ,&nbsp;&nbsp;{age()} {t('unit.yearsold')}
+        </Show>
+      </span>
+    </div>
+  )
+}
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -7,3 +7,23 @@
 export function addYearsToDate(years: number, date: Date = new Date()) {
   return new Date((new Date()).setFullYear((date).getFullYear() + years))
 }
+
+/**
+ * Calculates difference between two dates in ms
+ * @param {Date} higher - The higher date (the newer one)
+ * @param {Date} lower  - The higher date (the older one)
+ * @returns {number}
+ */
+export function getDiffInMs(higher: Date, lower: Date) {
+  return higher.getTime() - lower.getTime()
+}
+
+/**
+ * Calculates difference between two dates in years
+ * @param {Date} higher - The higher date (the newer one)
+ * @param {Date} lower  - The higher date (the older one)
+ * @returns {number} Non-integer value
+ */
+export function getDiffInYears(higher: Date, lower: Date) {
+  return getDiffInMs(higher, lower) / (1000 * 60 * 60 * 24 * 365.25)
+}
