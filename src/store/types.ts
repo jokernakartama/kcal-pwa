60d37c6ef1cd5a5bf936253c2a927740caf20222
zzap <s.kobzev@yahoo.com>
2023-02-12 22:17:22 +0300

    Add meals to the store

--- src/store/types.ts
+++ src/store/types.ts
@@ -2,6 +2,7 @@ import { SetStoreFunction } from 'solid-js/store'
 
 export interface AppStoreState {
   journal?: DataModel.JournalRecord
+  meals: DataModel.Meal[]
   user?: UserModel.User
   goals?: UserModel.Goals
   info?: UserModel.Info
