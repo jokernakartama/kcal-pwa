4c67e0f296b0f933e31d7819571cd87e50e0b31b
zzap <s.kobzev@yahoo.com>
2023-02-28 21:34:14 +0300

    Add ability to justify content of ButtonPanel

--- src/components/ui/ButtonPanel/ButtonPanel.tsx
+++ src/components/ui/ButtonPanel/ButtonPanel.tsx
@@ -3,18 +3,26 @@ import { JSX, ParentComponent, splitProps } from 'solid-js'
 import styles from './styles.sass'
 
 type ButtonPanelComponent = ParentComponent<
-JSX.IntrinsicElements['div'] & { disabled?: boolean }
+JSX.IntrinsicElements['div'] & {
+  justify?: 'start' | 'center' | 'end'
+}
 >
 
 /**
  * Renders a button wrapper panel
  */
 export const ButtonPanel: ButtonPanelComponent = props => {
-  const [local, rest] = splitProps(props, ['class', 'children'])
+  const [local, rest] = splitProps(props, ['class', 'children', 'justify'])
 
   return (
     <div
-      class={classNames(styles.wrapper, local.class)}
+      class={
+        classNames(
+          styles.wrapper,
+          local.class,
+          { [styles[`justify-${local.justify ?? 'end'}`]]: true }
+        )
+      }
       {...rest}
     >
       {local.children}
