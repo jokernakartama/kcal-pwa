191293843aefb004e58d48181c251e1dbe77db84
zzap <s.kobzev@yahoo.com>
2023-03-07 21:46:35 +0300

    Add a button to clear TextInput

--- src/components/ui/TextInput/TextInput.tsx
+++ src/components/ui/TextInput/TextInput.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames'
-import { Component, JSX, splitProps } from 'solid-js'
+import { Component, JSX, Show, splitProps } from 'solid-js'
 import { emoji } from '../../../constants/emoji'
+import { CrossIcon } from '../../icons/CrossIcon'
 import styles from './styles.sass'
 import { TextInputTypeValue } from './types'
 
@@ -8,6 +9,23 @@ type TextInputComponent = Component<
 JSX.IntrinsicElements['input'] & {
   type: TextInputTypeValue
   icon?: keyof typeof emoji
+  /**
+   * Whether a button to clear the input should be visible.
+   * This action DOESN'T dispatch a fully identical to
+   * the original "input" event. The complete list of changes:
+   * - currentTarget: null
+   * - bubbles: false
+   * - explicitOriginalTarget is an HTMLDivElement
+   *   instead of the original input element
+   * - isTrusted: false
+   * - view: null
+   * - data: "" (empty string)
+   * - ...and some specific to synthetic events properties
+   *
+   * In case when you need original event subscribe
+   * to the "change" event instead
+   */
+  clearable?: boolean
 }
 >
 
@@ -15,12 +33,31 @@ JSX.IntrinsicElements['input'] & {
  * Renders various input fields
  */
 export const TextInput: TextInputComponent = props => {
-  const [local, rest] = splitProps(props, ['class', 'icon'])
+  const [local, rest] = splitProps(props, ['class', 'icon', 'clearable'])
+  let inputElement: HTMLInputElement
+
+  function handleClear() {
+    if (inputElement) {
+      inputElement.value = ''
+
+      if (typeof props.onInput === 'function') {
+        const evt = new InputEvent('input', { data: '', inputType: 'insertText' })
+
+        inputElement.dispatchEvent(evt)
+
+        props.onInput(evt as InputEvent & {
+          currentTarget: HTMLInputElement
+          target: Element
+        })
+      }
+    }
+  }
 
   return (
     <label
       class={classNames(styles.wrapper, local.class, {
-        [styles.disabled]: rest.disabled
+        [styles.disabled]: rest.disabled,
+        [styles.clearable]: local.clearable
       })}
     >
       {local.icon !== undefined && (
@@ -28,8 +65,14 @@ export const TextInput: TextInputComponent = props => {
           {emoji[local.icon].html}{' '}
         </div>
       )}
-      <input {...rest} />
+      <input ref={el => { inputElement = el }} {...rest} />
       <div class={styles.placeholder}>{rest.placeholder}</div>
+
+      <Show when={local.clearable}>
+        <div class={styles.clear} onClick={handleClear}>
+          <CrossIcon />
+        </div>
+      </Show>
     </label>
   )
 }
