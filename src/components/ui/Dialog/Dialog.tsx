9e56bee5b7bffae7babaf1861c824b8940695753
zzap <s.kobzev@yahoo.com>
2023-04-02 19:12:50 +0300

    Make Dialog closable on mobile devices

--- src/components/ui/Dialog/Dialog.tsx
+++ src/components/ui/Dialog/Dialog.tsx
@@ -1,5 +1,6 @@
 import classNames from 'classnames'
-import { JSX, ParentComponent, Show, splitProps } from 'solid-js'
+import { createEffect, createMemo, JSX, ParentComponent, Show, splitProps } from 'solid-js'
+import { createMovementCapture } from '../../../hooks/createMovementCapture'
 import { BackIcon } from '../../icons/BackIcon'
 import { CrossIcon } from '../../icons/CrossIcon'
 import styles from './styles.sass'
@@ -13,14 +14,23 @@ JSX.IntrinsicElements['div'] & {
 }
 >
 
+const TRESHOLD = .7
+
 /**
  * Renders dialog layout
  */
 export const Dialog: DialogComponent = props => {
+  let dialogElement: HTMLDivElement | undefined
+  const { isCaptured, touch, y } = createMovementCapture({ x: false })
   const [local, rest] = splitProps(
     props,
     ['class', 'children', 'header', 'footer', 'onClose', 'onBack']
   )
+  const dialogStyle = createMemo(() => {
+    return isCaptured()
+      ? { transform: `translateY(${Math.max(y()!, 0)}px)` }
+      : undefined
+  })
 
   function handleClose() {
     if (typeof local.onClose === 'function') {
@@ -34,16 +44,33 @@ export const Dialog: DialogComponent = props => {
     }
   }
 
+  createEffect(() => {
+    const yValue = y()
+
+    if (
+      typeof yValue !== 'undefined' &&
+      typeof props.onClose === 'function' &&
+      !isCaptured() &&
+      yValue > (dialogElement?.clientHeight ?? 0) * TRESHOLD
+    ) {
+      handleClose()
+    }
+  })
+
   return (
     <>
       <div class={styles.backdrop}></div>
 
       <div
+        ref={el => { dialogElement = el }}
         class={classNames(styles.wrapper, local.class)}
         {...rest}
       >
-        <div class={styles.window}>
-          <div class={styles.header}>
+        <div
+          class={classNames(styles.window, { [styles.captured]: isCaptured() })}
+          style={dialogStyle()}
+        >
+          <div class={styles.header} onTouchStart={touch}>
             <span class={styles.back} onClick={handleBack}>
               <BackIcon />
             </span>
